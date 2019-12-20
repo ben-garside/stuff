5ee6d659b048ea9fda8ed045790d2ffe62763263
Benjamin Garside <abgarside@gmail.com>
2019-12-20 17:20:54 +0000

    Update addUser.ps1

--- addUser.ps1
+++ addUser.ps1
@@ -4,3 +4,20 @@ Param(
 )
 
 Add-LocalGroupMember -Group Administrators -Member "$domain\$username"
+
+# Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install VS Pro
+choco install visualstudio2019professional
+
+# Install Browsers
+choco install googlechrome
+choco install firefox
+
+#Install applications
+choco install postman
+choco install microsoftazurestorageexplorer
+choco install filezilla
+choco install servicebusexplorer
+choco install nscp
