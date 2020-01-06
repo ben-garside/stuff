e0f872e8314d1f723a88110b654eb3029fbb7aac
Benjamin Garside <abgarside@gmail.com>
2020-01-06 18:28:25 +0000

    Update addUser.ps1

--- addUser.ps1
+++ addUser.ps1
@@ -6,7 +6,7 @@ Param(
 Add-LocalGroupMember -Group Administrators -Member "$domain\$username"
 
 # Init and disks
-powershell -Command Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
+Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
 
 # Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
