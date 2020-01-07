173c981c8032cd1bea5ec82cd22502e2740a0a0f
Benjamin Garside <abgarside@gmail.com>
2020-01-07 12:14:42 +0000

    Create addUser_cn.ps1

---
+++ addUser_cn.ps1
@@ -0,0 +1,26 @@
+Param(
+    $username,
+    $doamin
+)
+
+Add-LocalGroupMember -Group Administrators -Member "$domain\$username"
+
+# Init and disks
+Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
+
+# Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install VS Pro
+choco install visualstudio2019professional -y
+
+# Install Browsers
+choco install googlechrome -y
+choco install firefox -y
+
+#Install applications
+choco install postman -y
+choco install filezilla -y
+choco install nscp -y
+choco install nodejs.install -y
+choco install git
