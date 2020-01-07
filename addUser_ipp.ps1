d4ebd8a110b93ad1fd0daf28a0e6075e01193370
Benjamin Garside <abgarside@gmail.com>
2020-01-07 12:19:12 +0000

    Create addUser_ipp.ps1

---
+++ addUser_ipp.ps1
@@ -0,0 +1,37 @@
+Param(
+    $username,
+    $doamin
+)
+
+Add-LocalGroupMember -Group Administrators -Member "$domain\$username"
+
+# Init and disks
+Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
+
+# Add windows features
+add-windowsfeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Http-Errors, Web-Static-Content, Web-Http-Redirect, Web-Health, Web-Http-Logging, Web-Custom-Logging, Web-Performance, Web-Stat-Compression, Web-Dyn-Compression, Web-Security, Web-Filtering, Web-Basic-Auth, Web-IP-Security, Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp-Net, Web-Asp-Net45, Web-Mgmt-Tools, Web-Mgmt-Console
+
+# Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install VS Pro
+choco install visualstudio2019professional -y
+choco install classic-shell -y
+
+# Install Browsers
+choco install googlechrome -y
+choco install firefox -y
+
+#Install applications
+choco install microsoftazurestorageexplorer -y
+choco install filezilla -y
+choco install nscp -y
+choco install notepadplusplus --force -y
+choco install winmerge -y
+
+# IIS stuff
+choco install iis-arr --force -y
+choco install urlrewrite --force -y
+
+# AAR
+Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.webServer/proxy" -name "enabled" -value "True"
