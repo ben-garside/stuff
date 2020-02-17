cbec5e9d005f954fe11cb0af9618b300455d2189
Benjamin Garside <abgarside@gmail.com>
2020-02-17 21:18:21 +0000

    Update addUser_ma.ps1

--- addUser_ma.ps1
+++ addUser_ma.ps1
@@ -46,21 +46,25 @@ function Invoke-BlobItems {
      }
 }
 
+write-host "#### START"
 $poop = "sv=2019-02-02&ss=b&srt=sco&sp=rl&se=2021-01-09T16:44:05Z&st=2020-01-09T08:44:05Z&spr=https&sig="
 $sas = "$poop$sas"
 
+write-host "#### Adding user to admin group"
 $AdminGroup = [ADSI]"WinNT://$env:computername/Administrators,group"
 $User = [ADSI]"WinNT://$domain/$username,user"
 $AdminGroup.Add($User.Path)
 
 # Init and disks
+write-host "#### Init disk and assigning Drive letter"
 Get-Disk | Where-Object partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -DriveLetter F -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false
 
 # Add windows features
+write-host "#### Adding windows features"
 add-windowsfeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Http-Errors, Web-Static-Content, Web-Http-Redirect, Web-Health, Web-Http-Logging, Web-Custom-Logging, Web-Performance, Web-Stat-Compression, Web-Dyn-Compression, Web-Security, Web-Filtering, Web-Basic-Auth, Web-IP-Security, Web-App-Dev, Web-Net-Ext, Web-Net-Ext45, Web-Asp-Net, Web-Asp-Net45, Web-Mgmt-Tools, Web-Mgmt-Console, NET-WCF-HTTP-Activation45
 
 # Install choco
-
+write-host "#### Installing Choco"
 if([System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProtocolType]::Tls12)){
     Write-Host "TLS 1.2 active"
 } else {
@@ -70,6 +74,7 @@ if([System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProto
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+write-host "#### Installling Apps via choco"
 # Install VS Pro
 choco install visualstudio2019professional -y
 choco install classic-shell -y
@@ -91,9 +96,11 @@ choco install iis-arr --force -y
 choco install urlrewrite --force -y
 
 # AAR
+write-host "#### Setting AAR"
 Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.webServer/proxy" -name "enabled" -value "True"
 
 # Disable IE ESC
+write-host "#### Disabling IE ESC"
 $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
 $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
@@ -101,6 +108,7 @@ Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
 Stop-Process -Name Explorer
 
 # Create folders
+write-host "#### Creating folders"
 New-Item -Path "F:\" -Name "Projects" -ItemType "directory" -Force
 New-Item -Path "F:\" -Name "SQL" -ItemType "directory" -Force
 New-Item -Path "F:\SQL" -Name "Data" -ItemType "directory" -Force
@@ -108,24 +116,29 @@ New-Item -Path "F:\SQL" -Name "Log" -ItemType "directory" -Force
 New-Item -Path "F:\SQL" -Name "Backups" -ItemType "directory" -Force
 
 # Get db backups
+write-host "#### Getting DB backups"
 $url = "$blob/$dbContainer"
 Invoke-BlobItems -uri $url -sas $sas -Path $path
-
 $url = "$blob/$vmContainer"
 Invoke-BlobItems -uri $url -sas $sas -Path $path
 
 # Rename backups
+write-host "#### Tidying DB names"
 $files = Get-ChildItem -Path $path -Filter *.bak
 $regex = "(.*_)(.*)(_.*)"
 
 # Update SQL auth mode to Mixed via REG and restart the service
+write-host "#### Updating SQL login mode"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQLServer" -Name LoginMode -Value 2
 Restart-Service -Name MSSQLSERVER
+sleep 10
 while((Get-Service -Name MSSQLSERVER).Status -ne 'running'){
+    write-host "# sleeping... "
     sleep 10
 }
 
 # Add SQL user
+write-host "#### Adding local SQL user"
 $command = 
 @"
 Invoke-Sqlcmd -Query "CREATE LOGIN [$dbUser] WITH PASSWORD=N'$dbPassword', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF; EXEC master..sp_addsrvrolemember @loginame = N'$dbUser', @rolename = N'sysadmin'"
@@ -138,16 +151,20 @@ $credential = New-Object System.Management.Automation.PSCredential "\$LocalAdmin
 Start-Process powershell.exe  -Credential $credential -ArgumentList ("-encodedCommand $encodedCommand")
 
 # Install EpiServer
+write-host "#### Installing EPISERVER"
 $epiPath = 'F:\SQL\Backups\EPI'
 7z e 'F:\SQL\Backups\EPiServer 7.5.394.2.7z' -o"$epiPath" -r -y
 cd $epiPath
 $msis = Get-ChildItem -Path $epiPath -Filter *.msi
 foreach($msi in $msis){
-   install-msi $msi
+   install-msi "$epiPath\$msi"
 }
 
 # Restore DBs
+write-host "#### Restoring Databases"
 $sqlrestore = "$blob/$vmContainer/restore.txt?$sas"
 $sqlcommand = (Invoke-webrequest -URI $sqlrestore -UseBasicParsing).Content
 Invoke-Sqlcmd -Query $sqlcommand -Username "$dbUser" -Password "$dbPassword" -ConnectionTimeout 0 -QueryTimeout 0
+
+write-host "#### END"
 
